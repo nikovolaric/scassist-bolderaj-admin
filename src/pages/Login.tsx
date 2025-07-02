eb3ad3e401af64c0fa079800205f53cb498bdd32
nikovolaric <niko.volaric@gmail.com>
2025-07-02 11:05:52 +0200

    Intital commit

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -15,7 +15,12 @@ function Login() {
 
   useEffect(
     function () {
-      if (!isPending && data.name !== "Error" && data.role.includes("admin")) {
+      if (
+        !isPending &&
+        data &&
+        data.name !== "Error" &&
+        data.role.includes("admin")
+      ) {
         navigate("/dashboard");
       }
     },
